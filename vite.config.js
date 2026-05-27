7c6d4cc13fdb79c3b44bf22cd8b22247acb3a51d
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-05-27 22:28:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,38 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: "0.0.0.0",
+//     allowedHosts: ["uniquebuilders.onrender.com"],
+//     proxy: {
+//       "/proxy-api": {
+//         target: "https://ub-admin-ejs-egx5.onrender.com",
+//         changeOrigin: true,
+//         secure: true,
+//         rewrite: (path) => path.replace(/^\/proxy-api/, ""),
+//       },
+//     },
+//   },
+//   preview: {
+//     host: "0.0.0.0",
+//     allowedHosts: ["uniquebuilders.onrender.com"],
+//   },
+// });
+
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     host: "0.0.0.0",
-    allowedHosts: ["uniquebuilders.onrender.com"],
+    allowedHosts: ["users-panel-for-building.onrender.com"],
+
     proxy: {
       "/proxy-api": {
         target: "https://ub-admin-ejs-egx5.onrender.com",
@@ -15,8 +42,9 @@ export default defineConfig({
       },
     },
   },
+
   preview: {
     host: "0.0.0.0",
-    allowedHosts: ["uniquebuilders.onrender.com"],
+    allowedHosts: ["users-panel-for-building.onrender.com"],
   },
 });
